35298598d436acce4e174867bd9c3b41d0e6a6bf
Timo4ey <tima_yakovishin@mail.ru>
2024-12-13 19:08:24 +0000

    feat: add docker compose and dockerfile

--- app/animals_dnk/animals_dnk/settings.py
+++ app/animals_dnk/animals_dnk/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-(raeh%t%s_)*#87wb7%btpm02e$bq8np(3y4y+_@lu_r6f%pwt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
 
 
 # Application definition
@@ -81,11 +81,15 @@ WSGI_APPLICATION = "animals_dnk.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": 'db',
+        "PORT": 5432,
+    },
 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
